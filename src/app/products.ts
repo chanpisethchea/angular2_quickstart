83aa1cdb19baad6de1be5deaabdbd3f66a82be4f
chanpisethchea <chpiseth9@gmail.com>
2017-08-31 15:59:44 -0500

    Added all project Angular 2

---
+++ src/app/products.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+   ProductID: number;
+   ProductName: string;
+}
+
+export class Product { 
+   constructor ( 
+      public productid: number, 
+      public productname: string
+   ) {  } 
+}
